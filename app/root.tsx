0678f73529b150ed943079a5bd14424f4b3a055a
DominikTomczyk <0641614@hc.ac.je>
2025-05-01 10:28:29 +0100

    Added images to files and added nav bar to root

--- app/root.tsx
+++ app/root.tsx
@@ -4,9 +4,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  Link,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
+import siteLogo from "~/assets/svg/gamelog-logo.svg";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -24,6 +26,16 @@ export default function App() {
       <body>
         <div className="flex flex-col min-h-screen bg-gray-950 text-gray-50">
           <div className="flex-1">
+            <nav className="flex justify-between px-10 py-5 items-center">
+              <div>
+              <Link to="/"><img src={siteLogo} alt="Gamelog Logo"/></Link>
+              </div>
+              <div className="flex gap-32">
+                <Link to="/games">Games</Link>
+                <Link to="/about">About</Link>
+                <Link to="/blog">Blog</Link>
+              </div>
+            </nav>
             <Outlet />
           </div>
           <ScrollRestoration />
--- app/routes/add_game.tsx
+++ app/routes/add_game.tsx
@@ -3,10 +3,9 @@ import type { MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => {
   return [
     { title: "Add Game" },
-    { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function AddGame() {
-    return <div className="container px-20 py-20 font-bold text-6xl text-center">Add <span className="text-cyan-500">Game</span></div>
+    return <div className="py-40 font-bold text-6xl text-center">Add <span className="text-cyan-500">Game</span></div>
 }
