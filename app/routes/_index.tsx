5598acb7b914a5f4258d6897746d13ef934cae5b
DominikTomczyk <0641614@hc.ac.je>
2025-05-30 11:04:33 +0100

    Updated hero image and text responsiveness

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -42,14 +42,14 @@ export default function Index() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="flex flex-col bg-[url(assets/png/hero-image.png)] bg-no-repeat bg-center w-[1368px] h-[697px] justify-between p-24">
-        <p className="text-[80px]/[100px] font-bold w-[550px] text-slate-200">Track Your <span className="bg-gradient-to-r from-cyan-300 to-cyan-600 text-transparent bg-clip-text">Gaming</span> Journey with Ease</p>
-        <Link to="/add_game" className="flex bg-transparent rounded-xl border-2 border-cyan-300 text-cyan-300 w-[630px] h-16 text-[18px] justify-center items-center">Add Game</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-clip">
+      <div className="flex flex-col bg-[url(assets/png/hero-image.png)] bg-no-repeat bg-cover w-full sm:w-[1368px] bg-center justify-between p-5 sm:p-40">
+        <p className="text-6xl sm:text-[80px]/[100px] font-bold w-3/5 sm:w-3/6 text-slate-200">Track Your <span className="bg-gradient-to-r from-cyan-300 to-cyan-600 text-transparent bg-clip-text">Gaming</span> Journey with Ease</p>
+        <Link to="/add_game" className="flex bg-transparent rounded-xl border-2 border-cyan-300 text-cyan-300 w-auto sm:w-[630px] h-16 text-[18px] justify-center items-center mt-16">Add Game</Link>
       </div>
-      <div className='flex flex-row justify-between w-[1368px] h-[50px] items-center'>
-        <p className="text-slate-200 text-2xl font-semibold">Games</p>
-        <Link to="/games" className="text-slate-200 text-sm">See all</Link>
+      <div className='flex flex-row justify-between w-full sm:w-[1368px] h-[50px] items-center p-5 sm:p-0'>
+        <p className="text-slate-200 text-3xl sm:text-2xl font-semibold">Games</p>
+        <Link to="/games" className="text-slate-200 sm:text-sm">See all</Link>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
         {topGames.map((game) => <GameCard key={game.id} id={game.id} title={game.title} released={game.releaseDate} genre={game.category?.title || "No category"} imgUrl={game.imageUrl || siteLogo} page={"0"}/>)}
