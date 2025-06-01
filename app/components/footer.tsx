8d9380835abe93562f1f98c6681e256552a40d40
DominikTomczyk <0641614@hc.ac.je>
2025-06-01 15:38:30 +0100

    Updated footer responsiveness

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,16 +2,17 @@ import { Link } from "@remix-run/react";
 import siteLogo from "~/assets/svg/gamelog-logo.svg";
 
 export default function Footer() {
-    return <nav className="flex justify-between px-10 py-5 items-center">
-        <div className="flex flex-col">
-            <Link to="/"><img src={siteLogo} alt="Gamelog Logo" className="w-[4rem] h-auto"/></Link>
-            <div className="flex gap-4 mt-3">
-                <Link to="/"><img src="app/assets/svg/facebook.svg" alt="Facebook Logo" className="w-[2rem] h-auto"/></Link>
-                <Link to="/"><img src="app/assets/svg/instagram.svg" alt="Instagram Logo" className="w-[2rem] h-auto"/></Link>
-                <Link to="/"><img src="app/assets/svg/twitter.svg" alt="Twitter Logo" className="w-[2rem] h-auto"/></Link>
+    return <footer className="flex justify-center sm:justify-between px-10 py-5 items-center">
+        <div className="flex flex-col items-center sm:items-start">
+            <Link to="/" className="hidden sm:flex"><img src={siteLogo} alt="Gamelog Logo" className="w-[4rem] h-auto"/></Link>
+            <img src={siteLogo} alt="Gamelog Logo" className="w-28 h-auto sm:hidden mb-5"/>
+            <div className="flex gap-28 sm:gap-4 mt-3">
+                <Link to="/"><img src="app/assets/svg/facebook.svg" alt="Facebook Logo" className="w-12 sm:w-[2rem] h-auto"/></Link>
+                <Link to="/"><img src="app/assets/svg/instagram.svg" alt="Instagram Logo" className="w-12 sm:w-[2rem] h-auto"/></Link>
+                <Link to="/"><img src="app/assets/svg/twitter.svg" alt="Twitter Logo" className="w-12 sm:w-[2rem] h-auto"/></Link>
             </div>
         </div>
-        <div className="flex gap-32 pr-32">
+        <div className="sm:flex gap-32 pr-32 hidden">
             <ul>
                 <li><p className="text-slate-200 text-sm font-semibold">Site</p></li>
                 <li><Link to="/games" className="text-gray-400 text-sm">Games</Link></li>
@@ -31,5 +32,5 @@ export default function Footer() {
                 <li><Link to="/" className="text-gray-400 text-sm">Instagram</Link></li>
             </ul>
         </div>
-    </nav>
+    </footer>
 }
